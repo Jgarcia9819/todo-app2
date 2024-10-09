72df05cad1a79589e722660872f84a1c726f704f
Jgarcia9819 <138169448+Jgarcia9819@users.noreply.github.com>
2024-10-08 18:25:42 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // Ensure the base path is correctly set
+  root: ".", // Set this to your project root where index.html is located
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },
-  build: {
-    outDir: "dist", // Ensure the output directory is set correctly
-  },
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/auth", "firebase/database"],
   },
 });
